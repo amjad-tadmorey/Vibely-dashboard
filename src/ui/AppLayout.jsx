d2cd96ed18deb41129257c1bee988294b74d1553
penko <amjadtadmory@gmail.com>
2025-07-26 16:20:44 +0300

    .

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -3,26 +3,11 @@ import ThumbNav from "./ThumbNav";
 import Header from "./Header";
 import { shop_id } from "../constants/local";
 import { useEffect } from "react";
-import { useAuth } from "../context/AuthContext";
 
 export default function AppLayout() {
-  const { user } = useAuth();
-  const role = user.user_metadata.role
-
-
   useEffect(() => {
     if (shop_id === null) window.location.reload()
   })
-
-  window.onerror = function (message, source, lineno, colno, error) {
-    document.body.innerHTML += `
-    <div style="color: red; background: #ffe6e6; padding: 1rem;">
-      Error: ${message}<br>
-      Source: ${source}<br>
-      Line: ${lineno}, Column: ${colno}
-    </div>
-  `;
-  };
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-50 to-gray-100 text-gray-800 dark:bg-black">
